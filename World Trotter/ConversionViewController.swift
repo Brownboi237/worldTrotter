0884af2eb0e496d450822a00fcd182de239eede9
Marco Aguilar <marcoaguilar@marcos-air.attlocal.net>
2017-09-14 07:05:48 -0500

    first commit

---
+++ World Trotter/ConversionViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ConversionViewController.swift
+//  World Trotter
+//
+//  Created by Marco Aguilar on 9/1/17.
+//  Copyright © 2017 Brown Space Mango. All rights reserved.
+//
+
+import Foundation
---
+++ World Trotter/MapViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MapViewController.swift
+//  World Trotter
+//
+//  Created by Marco Aguilar on 9/11/17.
+//  Copyright © 2017 Brown Space Mango. All rights reserved.
+//
+
+import Foundation
